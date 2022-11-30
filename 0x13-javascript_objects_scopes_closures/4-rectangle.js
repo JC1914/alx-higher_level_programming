8dabdeb685b2595d22464fa36a8796daa33cae4a
JC1914 <105009461+JC1914@users.noreply.github.com>
2022-11-30 22:48:20 +0300

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,31 @@
 #!/usr/bin/node
-// JS script to define a class, creates an instance method to print rectangle
+/* Task 4 - Write a class Rectangle that defines a rectangle */
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
+    /* If w or h is equal to 0 or not a positive integer, create an empty object */
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print () {
-    for (let count = 0; count < this.height; count++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let holder = this.width;
-    this.width = this.height;
-    this.height = holder;
-  }
-  double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
-  }
 }
+
+/* Create an instance method called print() that prints the rectangle using the character X */
+Rectangle.prototype.print = function () {
+  for (let i = 0; i < this.height; i++) {
+    console.log('X'.repeat(this.width));
+  }
+};
+
+/* Create an instance method called rotate() that exchanges the width and the height of the rectangle */
+Rectangle.prototype.rotate = function () {
+  [this.width, this.height] = [this.height, this.width];
+};
+
+/* Create an instance method called double() that multiples the width and the height of the rectangle by 2 */
+Rectangle.prototype.double = function () {
+  this.width *= 2;
+  this.height *= 2;
+};
+
 module.exports = Rectangle;
